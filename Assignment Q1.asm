e8ba6b5ccb242e7a4613e9c8a574df91f016c08a
Nazmus Sakib Apurba <nazmussakibapurbo@gmail.com>
2022-11-24 23:28:18 +0600

    3rd commit

--- Assignment Q1.asm
+++ Assignment Q1.asm
@@ -10,11 +10,11 @@
 
     
 
-     MOV AH, 2                    ; display the character stored in BL   
+     MOV AH, 2                    ; display the character stored in BL
      MOV DL, BL
      INT 21H
 
-     MOV AH, 4CH                 ; exit
+     MOV AH, 4CH                 
      INT 21H
 
    MAIN ENDP
